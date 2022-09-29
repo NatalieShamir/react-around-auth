2c16fde7733957be9f6eff8402ae3ba9d53c8e09
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-09-29 12:56:42 +0300

    import ImagePopup component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Main from "./Main";
 import { Footer } from "./Footer";
+import { ImagePopup } from "./ImagePopup";
 
 function App() {
   return (
@@ -166,22 +167,7 @@ function App() {
           </form>
         </div>
       </div>
-      <div className="popup popup_type_preview">
-        <div className="popup__container popup__container_content_preview">
-          <figure className="popup__image-content">
-            <button
-              type="button"
-              className="popup__close-button popup__close-button_close_preview"
-            ></button>
-            <img
-              className="popup__preview-image"
-              src="#"
-              alt="popup featuring an enlarged gallery image"
-            />
-            <figcaption className="popup__preview-caption"></figcaption>
-          </figure>
-        </div>
-      </div>
+      <ImagePopup />
       <template id="card-template">
         <li className="card">
           <div className="card__image" syle=""></div>
