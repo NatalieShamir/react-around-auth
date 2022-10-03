8cd7c634710bf67322574f348b25e351876ab217
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-10-03 12:09:20 +0300

    transfer the content which is different for the four popups with form using children props

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,112 @@ function App() {
       <Header />
       <Main />
       <Footer />
-      <PopupWithForm name="edit-profile" title="Edit profile" />
-      <PopupWithForm name="add-card" title="New Place" />
-      <PopupWithForm name="avatar-change" title="Change profile picture" />
-      <PopupWithForm name="confirm-delete" title="Are you sure?" />
+      <PopupWithForm name="edit-profile" title="Edit profile">
+        {" "}
+        <fieldset class="popup__form-fieldset">
+          <label for="name" class="popup__form-label"></label>
+          <input
+            type="text"
+            id="name-input"
+            class="popup__form-input popup__form-input_type_name"
+            name="name"
+            placeholder="Name"
+            required
+            minlength="2"
+            maxlength="40"
+          />
+          <span id="name-input-error" class="popup__form-error"></span>
+        </fieldset>
+        <fieldset class="popup__form-fieldset">
+          <label for="job" class="popup__form-label"></label>
+          <input
+            type="text"
+            id="job-input"
+            class="popup__form-input popup__form-input_type_job"
+            name="job"
+            placeholder="About me"
+            required
+            minlength="2"
+            maxlength="200"
+          />
+          <span id="job-input-error" class="popup__form-error"></span>
+        </fieldset>
+        <fieldset class="popup__form-fieldset">
+          <button type="submit" class="button popup__form-button">
+            Save
+          </button>
+        </fieldset>
+      </PopupWithForm>
+      <PopupWithForm name="add-card" title="New Place">
+        {" "}
+        <fieldset class="popup__form-fieldset">
+          <label for="title" class="popup__form-label"></label>
+          <input
+            type="text"
+            id="title-input"
+            class="popup__form-input"
+            name="title"
+            placeholder="Title"
+            required
+            minlength="1"
+            maxlength="30"
+          />
+          <span id="title-input-error" class="popup__form-error"></span>
+        </fieldset>
+        <fieldset class="popup__form-fieldset">
+          <label for="image" class="popup__form-label"></label>
+          <input
+            type="url"
+            id="image-input"
+            class="popup__form-input popup__form-input_type_image"
+            name="image"
+            placeholder="Image URL"
+            required
+          />
+          <span id="image-input-error" class="popup__form-error"></span>
+        </fieldset>
+        <fieldset class="popup__form-fieldset">
+          <button
+            type="submit"
+            class="button popup__form-button popup__form-button_add-card"
+          >
+            Create
+          </button>
+        </fieldset>
+      </PopupWithForm>
+      <PopupWithForm name="avatar-change" title="Change profile picture">
+        <fieldset class="popup__form-fieldset">
+          <label for="image" class="popup__form-label"></label>
+          <input
+            type="url"
+            id="avatar-input"
+            class="popup__form-input popup__form-input_type_image"
+            name="image"
+            placeholder="Image URL"
+            required
+          />
+          <span id="avatar-input-error" class="popup__form-error"></span>
+        </fieldset>
+        <fieldset class="popup__form-fieldset">
+          <button
+            type="submit"
+            class="button popup__form-button popup__form-button_avatar-change"
+          >
+            Save
+          </button>
+        </fieldset>
+      </PopupWithForm>
+      <PopupWithForm name="confirm-delete" title="Are you sure?">
+        {" "}
+        <fieldset class="popup__form-fieldset">
+          <button
+            type="submit"
+            class="button popup__form-button popup__form-button_confirm-delete"
+          >
+            Yes
+          </button>
+        </fieldset>
+      </PopupWithForm>
       <ImagePopup />
       <template id="card-template">
         <li className="card">
