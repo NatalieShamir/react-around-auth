d9638d2642bbdb369eb18a7881f5f684194866e6
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-10-08 14:52:41 +0300

    add the isOpen prop

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,17 +1,22 @@
+let isOpen = false;
+
 export default function PopupWithForm(props) {
   return (
-    <div className={`popup popup_type_${props.name}`}>
-      <div className="popup__container">
-        <button
-          type="button"
-          aria-label="close"
-          className="popup__close-button"
-        ></button>
-        <form name={props.name} className="popup__form">
-          <h3 className="popup__form-title">{props.title}</h3>
-          {props.children}
-        </form>
+    <>
+      <div className={`popup popup_type_${props.name}`}>
+        <div className="popup__container">
+          <button
+            type="button"
+            aria-label="close"
+            className="popup__close-button"
+          ></button>
+          <form name={props.name} className="popup__form">
+            <h3 className="popup__form-title">{props.title}</h3>
+            {props.children}
+          </form>
+        </div>
       </div>
-    </div>
+      <div className={isOpen ? "popup_open" : ""}></div>
+    </>
   );
 }
