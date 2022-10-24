e8814361624750c5b2c9e3f129b9f82451c2bdd8
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-10-24 11:25:53 +0300

    create Card component

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+export function Card(props) {
+  return (
+    <div className="card">
+      <div
+        className="card__image"
+        style={{ backgroundImage: `url(${props.card.link})` }}
+      ></div>
+      <button type="button" className="card__delete-button"></button>
+      <div className="card__info">
+        <h2 className="card-title">{props.card.name}</h2>
+        <div className="card__likes">
+          <button type="button" className="card__like-button"></button>
+          <div className="card__likes-amount">{props.card.likes.length}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
