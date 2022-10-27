4f5bd8c40f1735ce9a374dce68409c7f224003a1
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-10-27 14:39:50 +0300

    utilize card props and add onClose props

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,18 +1,25 @@
-export default function ImagePopup() {
-  <div className="popup popup_type_preview">
-    <div className="popup__container popup__container_content_preview">
-      <figure className="popup__image-content">
-        <button
-          type="button"
-          className="popup__close-button popup__close-button_close_preview"
-        ></button>
-        <img
-          className="popup__preview-image"
-          src="#"
-          alt="popup featuring an enlarged gallery image"
-        />
-        <figcaption className="popup__preview-caption"></figcaption>
-      </figure>
+export default function ImagePopup({ card, onClose }) {
+  return (
+    <div
+      className={`popup popup_type_preview ${card.link ? "popup_open" : ""}`}
+    >
+      <div className="popup__container popup__container_content_preview">
+        <figure className="popup__image-content">
+          <button
+            type="button"
+            className="popup__close-button popup__close-button_close_preview"
+            onClick={onClose}
+          ></button>
+          <img
+            className="popup__preview-image"
+            src={card.link}
+            alt="popup featuring an enlarged gallery image"
+          />
+          <figcaption className="popup__preview-caption">
+            {card.name}
+          </figcaption>
+        </figure>
+      </div>
     </div>
-  </div>;
+  );
 }
