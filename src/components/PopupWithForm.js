8d258c8b73562fd36debd3baee15fae9dc0460b3
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-10-27 12:48:20 +0300

    delete redundant <></>

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,25 +1,23 @@
 function PopupWithForm(props) {
   return (
-    <>
-      <div
-        className={`${`popup popup_type_${props.name}`} ${
-          props.isOpen ? "popup_open" : ""
-        }`}
-      >
-        <div className="popup__container">
-          <button
-            onClick={props.onClose}
-            type="button"
-            aria-label="close"
-            className="popup__close-button"
-          ></button>
-          <form name={props.name} className="popup__form">
-            <h3 className="popup__form-title">{props.title}</h3>
-            {props.children}
-          </form>
-        </div>
+    <div
+      className={`${`popup popup_type_${props.name}`} ${
+        props.isOpen ? "popup_open" : ""
+      }`}
+    >
+      <div className="popup__container">
+        <button
+          onClick={props.onClose}
+          type="button"
+          aria-label="close"
+          className="popup__close-button"
+        ></button>
+        <form name={props.name} className="popup__form">
+          <h3 className="popup__form-title">{props.title}</h3>
+          {props.children}
+        </form>
       </div>
-    </>
+    </div>
   );
 }
 
