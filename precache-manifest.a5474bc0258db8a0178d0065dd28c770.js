30c5803e2fa97f3d6bd859c900f344cbcef98d69
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-10-31 12:09:47 +0200

    Updates

---
+++ precache-manifest.a5474bc0258db8a0178d0065dd28c770.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "135bd8505afb10dfb0aa",
+    "url": "/around-react/static/css/main.343241fc.chunk.css"
+  },
+  {
+    "revision": "135bd8505afb10dfb0aa",
+    "url": "/around-react/static/js/main.dd5a7004.chunk.js"
+  },
+  {
+    "revision": "3b8c460d310ce4c46639",
+    "url": "/around-react/static/js/runtime~main.09ee59de.js"
+  },
+  {
+    "revision": "c34e43393ef1d5c7e186",
+    "url": "/around-react/static/js/2.242c9b43.chunk.js"
+  },
+  {
+    "revision": "8fa34ba00032514f3d70",
+    "url": "/around-react/static/js/3.490731cf.chunk.js"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/around-react/static/media/header.8f7611ae.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/around-react/static/media/edit-profile-icon.d476a8b8.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/around-react/static/media/button-edit.6bfdb354.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/around-react/static/media/plus_sign.bdfb8ebb.svg"
+  },
+  {
+    "revision": "03a0aa43ae83b56ee544acdfb4aa1cc4",
+    "url": "/around-react/static/media/inter-v8-latin-500.03a0aa43.woff2"
+  },
+  {
+    "revision": "1f6623cb67fb5d9d205cb2e19b5933b7",
+    "url": "/around-react/static/media/inter-v8-latin-regular.1f6623cb.woff"
+  },
+  {
+    "revision": "7872d2ba383103a40a7cf9f23e9fd9b0",
+    "url": "/around-react/static/media/inter-v8-latin-900.7872d2ba.woff2"
+  },
+  {
+    "revision": "3a92ec2488a578522056ab029eeb71a1",
+    "url": "/around-react/static/media/inter-v8-latin-regular.3a92ec24.woff2"
+  },
+  {
+    "revision": "5397ee007185aea5345decda9935f1c8",
+    "url": "/around-react/static/media/inter-v8-latin-900.5397ee00.woff"
+  },
+  {
+    "revision": "63677a04d4c46c5ab4a7e89880ab0a43",
+    "url": "/around-react/static/media/inter-v8-latin-500.63677a04.woff"
+  },
+  {
+    "revision": "21098c48b6af8588c1b644f2653503f8",
+    "url": "/around-react/index.html"
+  }
+];
