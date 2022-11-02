a81f447ab2aeeb56b9d6beb32f2a04c853a23901
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-11-02 12:57:40 +0200

    apply camelCase property naming convention to class minlength and maxlength HTML attribute names

--- src/components/App.js
+++ src/components/App.js
@@ -59,36 +59,36 @@ function App() {
         onClose={closeAllPopups}
       >
         {" "}
-        <fieldset class="popup__form-fieldset">
-          <label for="name" class="popup__form-label"></label>
+        <fieldset className="popup__form-fieldset">
+          <label for="name" className="popup__form-label"></label>
           <input
             type="text"
             id="name-input"
-            class="popup__form-input popup__form-input_type_name"
+            className="popup__form-input popup__form-input_type_name"
             name="name"
             placeholder="Name"
             required
-            minlength="2"
-            maxlength="40"
+            minLength="2"
+            maxLength="40"
           />
-          <span id="name-input-error" class="popup__form-error"></span>
+          <span id="name-input-error" className="popup__form-error"></span>
         </fieldset>
-        <fieldset class="popup__form-fieldset">
-          <label for="job" class="popup__form-label"></label>
+        <fieldset className="popup__form-fieldset">
+          <label for="job" className="popup__form-label"></label>
           <input
             type="text"
             id="job-input"
-            class="popup__form-input popup__form-input_type_job"
+            className="popup__form-input popup__form-input_type_job"
             name="job"
             placeholder="About me"
             required
-            minlength="2"
-            maxlength="200"
+            minLength="2"
+            maxLength="200"
           />
-          <span id="job-input-error" class="popup__form-error"></span>
+          <span id="job-input-error" className="popup__form-error"></span>
         </fieldset>
-        <fieldset class="popup__form-fieldset">
-          <button type="submit" class="button popup__form-button">
+        <fieldset className="popup__form-fieldset">
+          <button type="submit" className="button popup__form-button">
             Save
           </button>
         </fieldset>
@@ -100,36 +100,36 @@ function App() {
         onClose={closeAllPopups}
       >
         {" "}
-        <fieldset class="popup__form-fieldset">
-          <label for="title" class="popup__form-label"></label>
+        <fieldset className="popup__form-fieldset">
+          <label for="title" className="popup__form-label"></label>
           <input
             type="text"
             id="title-input"
-            class="popup__form-input"
+            className="popup__form-input"
             name="title"
             placeholder="Title"
             required
-            minlength="1"
-            maxlength="30"
+            minLength="1"
+            maxLength="30"
           />
-          <span id="title-input-error" class="popup__form-error"></span>
+          <span id="title-input-error" className="popup__form-error"></span>
         </fieldset>
-        <fieldset class="popup__form-fieldset">
-          <label for="image" class="popup__form-label"></label>
+        <fieldset className="popup__form-fieldset">
+          <label for="image" className="popup__form-label"></label>
           <input
             type="url"
             id="image-input"
-            class="popup__form-input popup__form-input_type_image"
+            className="popup__form-input popup__form-input_type_image"
             name="image"
             placeholder="Image URL"
             required
           />
-          <span id="image-input-error" class="popup__form-error"></span>
+          <span id="image-input-error" className="popup__form-error"></span>
         </fieldset>
-        <fieldset class="popup__form-fieldset">
+        <fieldset className="popup__form-fieldset">
           <button
             type="submit"
-            class="button popup__form-button popup__form-button_add-card"
+            className="button popup__form-button popup__form-button_add-card"
           >
             Create
           </button>
@@ -141,22 +141,22 @@ function App() {
         isOpen={isEditAvatarPopupOpen}
         onClose={closeAllPopups}
       >
-        <fieldset class="popup__form-fieldset">
-          <label for="image" class="popup__form-label"></label>
+        <fieldset className="popup__form-fieldset">
+          <label for="image" className="popup__form-label"></label>
           <input
             type="url"
             id="avatar-input"
-            class="popup__form-input popup__form-input_type_image"
+            className="popup__form-input popup__form-input_type_image"
             name="image"
             placeholder="Image URL"
             required
           />
-          <span id="avatar-input-error" class="popup__form-error"></span>
+          <span id="avatar-input-error" className="popup__form-error"></span>
         </fieldset>
-        <fieldset class="popup__form-fieldset">
+        <fieldset className="popup__form-fieldset">
           <button
             type="submit"
-            class="button popup__form-button popup__form-button_avatar-change"
+            className="button popup__form-button popup__form-button_avatar-change"
           >
             Save
           </button>
@@ -164,10 +164,10 @@ function App() {
       </PopupWithForm>
       <PopupWithForm name="confirm-delete" title="Are you sure?">
         {" "}
-        <fieldset class="popup__form-fieldset">
+        <fieldset className="popup__form-fieldset">
           <button
             type="submit"
-            class="button popup__form-button popup__form-button_confirm-delete"
+            className="button popup__form-button popup__form-button_confirm-delete"
           >
             Yes
           </button>
