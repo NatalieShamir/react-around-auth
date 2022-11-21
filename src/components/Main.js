d8041dd0fbc95f2ac21a8ff0345ebf4d5b2747ba
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-11-21 14:06:00 +0200

    rewrite content of handleCardLike() using addLike() and removeLike() api methods

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,17 +11,18 @@ export default function Main(props) {
   const [cards, setCards] = React.useState([]);
 
   function handleCardLike(card) {
-    // Check one more time if this card was already liked
     const isLiked = card.likes.some((user) => user._id === currentUser._id);
 
-    // Send a request to the API and getting the updated card data
-    api.changeLikeCardStatus(card._id, !isLiked).then((newCard) => {
-      setCards((state) =>
-        state.map((currentCard) =>
-          currentCard._id === card._id ? newCard : currentCard
-        )
-      );
-    });
+    if (isLiked) {
+      api.removeLike(card._id);
+    } else {
+      api.addLike(card._id).then((likedCard) => {
+        const newCards = cards.map((card) => {
+          return card._id == likedCard._id ? likedCard : card;
+        });
+        setCards(newCards);
+      });
+    }
   }
 
   function handleCardDelete(card) {
