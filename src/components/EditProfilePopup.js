f1e9dec74491e355cacbcb8c8430126416a8a525
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-11-24 13:23:57 +0200

    create EditProfilePopup component and move the corresponding <PopupWithForm> tag and its contents from App to it

---
+++ src/components/EditProfilePopup.js
@@ -0,0 +1,41 @@
+function EditProfilePopup() {
+  return (
+    <PopupWithForm
+      name="edit-profile"
+      title="Edit profile"
+      isOpen={isEditProfilePopupOpen}
+      onClose={closeAllPopups}
+      buttonText={"Save"}
+    >
+      {" "}
+      <fieldset className="popup__form-fieldset">
+        <label htmlFor="name" className="popup__form-label"></label>
+        <input
+          type="text"
+          id="name-input"
+          className="popup__form-input popup__form-input_type_name"
+          name="name"
+          placeholder="Name"
+          required
+          minLength="2"
+          maxLength="40"
+        />
+        <span id="name-input-error" className="popup__form-error"></span>
+      </fieldset>
+      <fieldset className="popup__form-fieldset">
+        <label htmlFor="job" className="popup__form-label"></label>
+        <input
+          type="text"
+          id="job-input"
+          className="popup__form-input popup__form-input_type_job"
+          name="job"
+          placeholder="About me"
+          required
+          minLength="2"
+          maxLength="200"
+        />
+        <span id="job-input-error" className="popup__form-error"></span>
+      </fieldset>
+    </PopupWithForm>
+  );
+}
