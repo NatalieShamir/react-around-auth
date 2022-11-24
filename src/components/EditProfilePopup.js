54c7f1691281f3a66b806785daf4280faa147a90
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-11-24 14:40:17 +0200

    add onChange() handlers to input fields

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -20,6 +20,7 @@ function EditProfilePopup({ isOpen, onClose }) {
           className="popup__form-input popup__form-input_type_name"
           name="name"
           value={name}
+          onChange={(e) => setName(e.target.value)}
           placeholder="Name"
           required
           minLength="2"
@@ -35,6 +36,7 @@ function EditProfilePopup({ isOpen, onClose }) {
           className="popup__form-input popup__form-input_type_job"
           name="job"
           value={description}
+          onChange={(e) => setDescription(e.target.value)}
           placeholder="About me"
           required
           minLength="2"
