d99cb3f5fc278d45d72fc15d69d5500a3a40ebe6
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-11-27 14:01:08 +0200

    import react

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { React, useState } from "react";
 
 function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
   const currentUser = React.useContext(CurrentUserContext);
