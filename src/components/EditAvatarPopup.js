007367b8ba8135a6e9654d9c4c81f922c5e9b9d2
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-11-28 13:30:31 +0200

    move contents of corresponding PopupWithForm tag from App.js

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,1 +1,24 @@
-function EditAvatarPopup() {}
+function EditAvatarPopup() {
+  return (
+    <PopupWithForm
+      name="avatar-change"
+      title="Change profile picture"
+      isOpen={isEditAvatarPopupOpen}
+      onClose={closeAllPopups}
+      buttonText={"Save"}
+    >
+      <fieldset className="popup__form-fieldset">
+        <label htmlFor="image" className="popup__form-label"></label>
+        <input
+          type="url"
+          id="avatar-input"
+          className="popup__form-input popup__form-input_type_image"
+          name="image"
+          placeholder="Image URL"
+          required
+        />
+        <span id="avatar-input-error" className="popup__form-error"></span>
+      </fieldset>
+    </PopupWithForm>
+  );
+}
