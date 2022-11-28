87055ade70464385bac977ec2deb2ac079c283b9
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-11-28 13:29:32 +0200

    replace PopupWithForm component containing edit avatar content with EditAvatarPopup

--- src/components/App.js
+++ src/components/App.js
@@ -113,26 +113,10 @@ function App() {
             <span id="image-input-error" className="popup__form-error"></span>
           </fieldset>
         </PopupWithForm>
-        <PopupWithForm
-          name="avatar-change"
-          title="Change profile picture"
+        <EditAvatarPopup
           isOpen={isEditAvatarPopupOpen}
           onClose={closeAllPopups}
-          buttonText={"Save"}
-        >
-          <fieldset className="popup__form-fieldset">
-            <label htmlFor="image" className="popup__form-label"></label>
-            <input
-              type="url"
-              id="avatar-input"
-              className="popup__form-input popup__form-input_type_image"
-              name="image"
-              placeholder="Image URL"
-              required
-            />
-            <span id="avatar-input-error" className="popup__form-error"></span>
-          </fieldset>
-        </PopupWithForm>
+        />
         <PopupWithForm
           name="confirm-delete"
           title="Are you sure?"
