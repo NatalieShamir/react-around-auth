7b4ff2801a8fe1cfa820559b34687958fbee56d4
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-11-30 11:18:48 +0200

    create AddPlacePopup component and move corresponding content from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -132,41 +132,7 @@ function App() {
           onClose={closeAllPopups}
           onUpdateUser={handleUpdateUser}
         />
-        <PopupWithForm
-          name="add-card"
-          title="New Place"
-          isOpen={isAddPlacePopupOpen}
-          onClose={closeAllPopups}
-          buttonText={"Create"}
-        >
-          {" "}
-          <fieldset className="popup__form-fieldset">
-            <label htmlFor="title" className="popup__form-label"></label>
-            <input
-              type="text"
-              id="title-input"
-              className="popup__form-input"
-              name="title"
-              placeholder="Title"
-              required
-              minLength="1"
-              maxLength="30"
-            />
-            <span id="title-input-error" className="popup__form-error"></span>
-          </fieldset>
-          <fieldset className="popup__form-fieldset">
-            <label htmlFor="image" className="popup__form-label"></label>
-            <input
-              type="url"
-              id="image-input"
-              className="popup__form-input popup__form-input_type_image"
-              name="image"
-              placeholder="Image URL"
-              required
-            />
-            <span id="image-input-error" className="popup__form-error"></span>
-          </fieldset>
-        </PopupWithForm>
+
         <EditAvatarPopup
           isOpen={isEditAvatarPopupOpen}
           onClose={closeAllPopups}
