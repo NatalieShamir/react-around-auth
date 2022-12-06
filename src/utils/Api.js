da3695a87eff673343e8068e237718589dff9afd
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-12-06 12:13:33 +0200

    add editProfile()

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -23,6 +23,17 @@ class Api {
     }).then(this._checkResponse);
   }
 
+  editProfile(name, about) {
+    return fetch(this._baseUrl + "/users/me", {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    }).then(this._checkResponse);
+  }
+
   createCard(name, link) {
     return fetch(this._baseUrl + "/cards", {
       method: "POST",
