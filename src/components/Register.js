bd6a974d9b04d1586ee039871beb72758511cd85
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-03-15 12:06:04 +0200

    add state vars and jsx

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Register = ({ handleSubmit }) => {
+function Register({ onRegister }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleRegister = () => {
+        onRegister(email, password)
+    }
+
     return (
-        <form onSubmit={handleSubmit}></form>
+        <div className="register">
+            <p className="register__welcome">
+                Sign up
+            </p>
+            <form className="register__form">
+                <label>
+                    Email
+                </label>
+                <input name="email" type="email" value={email}
+                    onChange={e => setEmail(e.target.value)} />
+                <label>
+                    Password
+                </label>
+                <input name="password" type="password" value={password}
+                    onChange={e => setPassword(e.target.value)} />
+            </form>
+            <div className="register__button-container">
+                <button onClick={handleRegister} className="register__link">Sign up</button>
+            </div>
+            {/* link to login page */}
+            <div className="register__signin">
+                <p>Already a member?</p>
+                <Link to="login" className="register__login-link">Log in here!</Link>
+            </div>
+        </div>
     )
 }
 
-export { Register }
+export { Register };
