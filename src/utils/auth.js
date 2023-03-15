28914a3ebc582a879322ba65aa065a54502b11b2
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-03-15 17:17:28 +0200

    add request parameters

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,10 +1,28 @@
-const BASE_URL = "https://register.nomoreparties.co"
+export const BASE_URL = "https://register.nomoreparties.co"
 
-const signup = () => { }
+export const signin = (password, email) => {
+    return fetch(`${BASE_URL}/signin`, {
+        method: "POST",
+        body: {
+            password, email
+        }
+    })
+}
 
-const signin = () => { }
+export const signup = (password, email) => {
+    return fetch(`${BASE_URL}/signup`, {
+        method: "POST",
+        body: {
+            password, email
+        }
+    })
+}
 
-const checkToken = () => { }
-
-
-
+export const checkToken = (token) => {
+    return fetch(`${BASE_URL}/users/me`, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    })
+}
