744820799e19254da301b8b996a50ebb58f403e5
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-03-15 15:31:21 +0200

    add state var

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Register({ onSubmit }) {
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
