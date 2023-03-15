f623f386331124c46d8d354e951d45adec053cec
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-03-15 12:59:01 +0200

    rewrite as functional component and add to jsx

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Login = ({ onLogin }) => {
+function Login({ onLogin }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -9,11 +9,27 @@ const Login = ({ onLogin }) => {
     }
 
     return (
-        <form className="login__form" onSubmit={handleLogin}>
-            <h3 className="login__form-title">Log in</h3>
-            <input value={email} onChange={e => setEmail(e.target.value)} />
-            <input value={password} onChange={e => setPassword(e.target.value)} />
-        </form>
+        <div className="login">
+            <p className="login__welcome">Log in</p>
+            <form className="login__form" onSubmit={handleLogin}>
+                <label for="email">Email</label>
+                <input value={email} onChange={e => setEmail(e.target.value)} />
+                <label for="password">Password</label>
+                <input value={password} onChange={e => setPassword(e.target.value)} />
+                <div className="login__button-container">
+                    <button type="submit" className="login__link">
+                        Log in
+                    </button>
+                </div>
+            </form>
+
+            <div className="login__signup">
+                <p>Not a member yet?</p>
+                <Link to="/register" className="signup__link">
+                    Sign up here!
+                </Link>
+            </div>
+        </div>
 
     )
 }
