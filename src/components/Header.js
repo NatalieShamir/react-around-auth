e73420a791100a429ffe1be40bf34a16afcf8100
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-03-21 12:04:12 +0200

    import and add Link component and functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import headerLogo from "../images/header.svg";
 import React from "react";
+import { Link, useLocation } from 'react-router-dom';
 
-export default function Header() {
+export default function Header({ isLoggedIn, email, signOut }) {
+
+  const location = useLocation();
+
   return (
     <header className="header">
       <img
@@ -9,6 +13,20 @@ export default function Header() {
         src={headerLogo}
         alt="Project title- Around The U.S."
       />
+
+      <div className="header__container">
+        {isLoggedIn ? (
+          <div>
+            <p className="header__email">{email}</p>
+            <div className="header__text">Log out</div>
+          </div>
+        ) : (
+          <div>
+            <Link to={location.pathname === "/signin" ? "/signup" : "/signin"} className="header__link"></Link>
+            {location.pathname === "/signin" ? "Sign up" : "Sign In"}
+          </div>
+        )}
+      </div>
     </header>
   );
 }
